7d8a8a6077b323c5c98adcbd36c51e82c58a319c
[dyad] <rinaldoautomacoes@gmail.com>
2025-10-21 08:28:54 -0300

    [dyad] wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-// import 'jspdf-autotable'; // Removido: A importação global não é mais necessária aqui
+import 'jspdf-autotable'; // Reativado: Garante que o plugin seja carregado
 
 createRoot(document.getElementById("root")!).render(<App />);
