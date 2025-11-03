e93f2448c7013edb5704ff0abcf1f883add4f7fd
[dyad] <rinaldoautomacoes@gmail.com>
2025-11-03 10:48:27 -0300

    [dyad] wrote 1 file(s)

--- src/components/ColetaForm.tsx
+++ src/components/ColetaForm.tsx
@@ -289,7 +289,7 @@ export const ColetaForm: React.FC<ColetaFormProps> = ({ initialData, onSave, onC
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         <div className="space-y-2">
-          <Label htmlFor="previsao_coleta">Data da Coleta *</Label>
+          <Label htmlFor="previsao_coleta">Previsão de Coleta *</Label> {/* Rótulo alterado aqui */}
           <Popover>
             <PopoverTrigger asChild>
               <Button
