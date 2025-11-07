fea6ddebbc696d6dded2f71f1ed72595a680c33f
[dyad] <rinaldoautomacoes@gmail.com>
2025-11-07 13:14:23 -0300

    [dyad] wrote 1 file(s) + extra files edited outside of Dyad

--- src/pages/ExcelExtractorPage.tsx
+++ src/pages/ExcelExtractorPage.tsx
@@ -6,7 +6,7 @@ import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { useToast } from '@/hooks/use-toast';
-import { useAuth } from '@/hooks/use-auth';
+import { useAuth } from '@/hooks/useAuth';
 import { generateUniqueNumber } from '@/lib/utils';
 import { format, parseISO, isValid } from 'date-fns';
 
